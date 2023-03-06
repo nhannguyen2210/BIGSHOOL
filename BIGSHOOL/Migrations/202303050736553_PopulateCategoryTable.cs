0fb89cd31af111a9e96e7499aefa543fd30d05be
Admin <Admin@ADMIN-PC>
2023-03-06 11:30:27 +0700

    Add project files.

---
+++ BIGSHOOL/Migrations/202303050736553_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace BIGSHOOL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO    categories(ID,NAME) VALUES (1,'Developmenty')");
+            Sql("INSERT INTO categories(ID, NAME) VALUES(2,'Business')");
+            Sql("INSERT INTO categories(ID,NAME) VALUES(3,'Maketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ BIGSHOOL/Models/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace BIGSHOOL.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }//khoá ngoại
+        [Required]
+       public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; } //khoá ngoai
+        [Required]
+        public byte CategoryId { get; set; }
+
+    }
+    //public class Category
+    //{
+    //    public byte Id { get; set; }
+    //    [Required,StringLength(255)]
+    //    public string Name { get; set; }    
+
+    //}
+
+}
